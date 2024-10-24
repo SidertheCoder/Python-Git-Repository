99a3215d9147d19e9cf1f7592d19c16d0ab1f2ce
S <siderthedoor@gmail.com>
2024-10-24 15:37:26 -0700

    Created empty grid to start the game

--- TTT.py
+++ TTT.py
@@ -1,2 +1,17 @@
 #TicTacToe
 
+def create_grid():
+    translation = {39: None}
+    grid_a1 = [' ']
+    grid_a2 = [' ']
+    grid_a3 = [' ']
+    grid_b1 = [' ']
+    grid_b2 = [' ']
+    grid_b3 = [' ']
+    grid_c1 = [' ']
+    grid_c2 = [' ']
+    grid_c3 = [' ']
+    print(str(grid_a1).translate(translation) + ' ' + str(grid_a2).translate(translation) + ' ' + str(grid_a3).translate(translation))
+    print(str(grid_b1).translate(translation) + ' ' + str(grid_b2).translate(translation) + ' ' + str(grid_b3).translate(translation))
+    print(str(grid_c1).translate(translation) + ' ' + str(grid_c2).translate(translation) + ' ' + str(grid_c3).translate(translation))
+
